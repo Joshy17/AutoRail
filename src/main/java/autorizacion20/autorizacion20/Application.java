174d3b9efa2347c79c44f935c1af62e4d1e49f89
Joshua Rosales Figueroa <joshuadrf@gmail.com>
2024-07-05 01:46:57 -0600

    Commit

--- src/main/java/autorizacion20/autorizacion20/Application.java
+++ src/main/java/autorizacion20/autorizacion20/Application.java
@@ -2,10 +2,18 @@ package autorizacion20.autorizacion20;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
+        @GetMapping("/prueba")
+    public String mensaje(){
+        return "hola";
+    }
+    
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
